57d9dce80fae8b519eb0cf9734cd95e54c0bca85
Justas <justaslescinskas@gmail.com>
2021-03-07 15:31:53 +0200

    Adding create player functionality and h2 database implementation

---
+++ src/main/java/entities/Player.java
@@ -0,0 +1,61 @@
+package entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Player.findAll", query = "select a from Player as a")
+})
+
+@Table(name = "PLAYER")
+public class Player implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 50)
+    @Column(name = "NAME")
+    private String name;
+
+    public Player() {
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(id, player.id) &&
+                Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+}
